42ab35351a744eb4094f9cde52bd1b02d7a3c8aa
EldarMemetov <eldarm9347@gmail.com>
2024-11-04 19:07:18 +0100

    change

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,79 +1,66 @@
-// const swiper = new Swiper('.swiper', {
-//   loop: false,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   slidesPerView: 6,
-//   spaceBetween: 48,
-//   centeredSlides: false,
-//   slideToClickedSlide: true,
-
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     1200: {
-//       slidesPerView: 6,
-//       spaceBetween: 48,
-//     },
-//   },
-// });
-
-// swiper.on('init', function () {
-//   updatePagination(swiper);
-// });
-
-// function updatePagination(swiper) {
-//   const pagination = swiper.pagination.bullets;
-//   pagination.each((bullet, index) => {
-//     if (index < swiper.slides.length) {
-//       bullet.style.display = 'block';
-//     } else {
-//       bullet.style.display = 'none';
-//     }
-//   });
-// }
-
-// swiper.init();
-
-const swiper = new Swiper('.swiper', {
-  loop: false,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  slidesPerView: 6,
-  spaceBetween: 48,
-  centeredSlides: true,
-  slideToClickedSlide: true,
-
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    loop: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
-    1200: {
-      slidesPerView: 6,
-      spaceBetween: 48,
+    slidesPerView: 6,
+    spaceBetween: 48,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 6,
+        spaceBetween: 48,
+      },
     },
-  },
-});
-
-swiper.on('init', function () {
-  updatePagination(swiper);
-});
-
-function updatePagination(swiper) {
-  const pagination = swiper.pagination.bullets;
-  pagination.each((bullet, index) => {
-    if (index < swiper.slides.length) {
-      bullet.style.display = 'block';
-    } else {
-      bullet.style.display = 'none';
-    }
   });
-}
+});
 
-swiper.init();
+// document.addEventListener('DOMContentLoaded', function () {
+//   const swiper = new Swiper('.swiper', {
+//     loop: false,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     slidesPerView: 6,
+//     spaceBetween: 48,
+//     centeredSlides: true,
+//     slideToClickedSlide: true,
+
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//       },
+//       1200: {
+//         slidesPerView: 6,
+//         spaceBetween: 48,
+//       },
+//     },
+//   });
+
+//   swiper.on('init', function () {
+//     updatePagination(swiper);
+//   });
+
+//   function updatePagination(swiper) {
+//     const pagination = swiper.pagination.bullets;
+//     pagination.each((bullet, index) => {
+//       if (index < swiper.slides.length) {
+//         bullet.style.display = 'block';
+//       } else {
+//         bullet.style.display = 'none';
+//       }
+//     });
+//   }
+
+//   swiper.init();
+// });
