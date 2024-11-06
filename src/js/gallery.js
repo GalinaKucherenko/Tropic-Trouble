2cba268a7752da21ba2e153864b7080d5e8e6899
EldarMemetov <eldarm9347@gmail.com>
2024-11-06 15:33:56 +0100

    change

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     breakpoints: {
       320: {
         slidesPerView: 2,
-        spaceBetween: 20,
+        spaceBetween: 30,
       },
       1200: {
         slidesPerView: 6,
