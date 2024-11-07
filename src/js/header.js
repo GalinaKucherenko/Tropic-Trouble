34bf1f2bc1686c20e206c6399f68ae11f44440b0
EldarMemetov <eldarm9347@gmail.com>
2024-11-07 09:33:37 +0100

    change-modal

--- src/js/header.js
+++ src/js/header.js
@@ -1,21 +1,48 @@
-document.getElementById('show-menu').addEventListener('click', function () {
-  document.getElementById('modal-menu').style.display = 'block';
-});
+// document.getElementById('show-menu').addEventListener('click', function () {
+//   document.getElementById('modal-menu').style.display = 'block';
+// });
 
-document.getElementById('close-modal').addEventListener('click', function () {
-  document.getElementById('modal-menu').style.display = 'none';
-});
+// document.getElementById('close-modal').addEventListener('click', function () {
+//   document.getElementById('modal-menu').style.display = 'none';
+// });
 
-document
-  .getElementById('modal-menu')
-  .addEventListener('click', function (event) {
-    if (event.target === this) {
-      this.style.display = 'none';
-    }
+// document
+//   .getElementById('modal-menu')
+//   .addEventListener('click', function (event) {
+//     if (event.target === this) {
+//       this.style.display = 'none';
+//     }
+//   });
+
+// document.querySelectorAll('.list-modal-nav a').forEach(link => {
+//   link.addEventListener('click', function () {
+//     document.getElementById('modal-menu').style.display = 'none';
+//   });
+// });
+document.addEventListener('DOMContentLoaded', () => {
+  const openMenuButton = document.getElementById('show-menu');
+  const closeMenuButton = document.getElementById('close-modal');
+  const modalMenu = document.getElementById('modal-menu');
+  const footer = modalMenu.querySelector('.footer-container');
+
+  const setFooterPosition = () => {
+    footer.style.position = 'absolute';
+    footer.style.bottom = '0';
+    footer.style.left = '0';
+  };
+
+  openMenuButton.addEventListener('click', () => {
+    modalMenu.style.display = 'flex';
+    setFooterPosition();
   });
 
-document.querySelectorAll('.list-modal-nav a').forEach(link => {
-  link.addEventListener('click', function () {
-    document.getElementById('modal-menu').style.display = 'none';
+  closeMenuButton.addEventListener('click', () => {
+    modalMenu.style.display = 'none';
+  });
+
+  window.addEventListener('resize', () => {
+    if (modalMenu.style.display === 'flex') {
+      setFooterPosition();
+    }
   });
 });
